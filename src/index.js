52ac2bac5ed467c98731b4fb6dfcc104a0aa8c03
April <Wanze.zhang.22@dartmouth.edu>
2020-04-17 00:23:27 -0400

    deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+// const $ = require('jquery');
+let num = 0;
+// eslint-disable-next-line no-useless-escape
+setInterval(() => { $('#main').html(`You\'\ ve been on this page for ${num} seconds.`); num += 1; }, 1000);
